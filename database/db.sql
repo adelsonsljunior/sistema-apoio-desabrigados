b86b7e2b70ebf889c4ee5047c468188988509bd8
adelsonsljunior <junioradelsonst@gmail.com>
2024-07-11 15:46:06 -0300

    feat: adding clothes to the database

--- database/db.sql
+++ database/db.sql
@@ -1,7 +1,28 @@
-CREATE TABLE IF NOT EXISTS distribution_centers(
+CREATE TABLE IF NOT EXISTS distribution_centers
+(
     id          SERIAL       NOT NULL,
     name        VARCHAR(120) NOT NULL,
     number      VARCHAR(8)   NOT NULL,
     postal_code VARCHAR(12)  NOT NULL,
     PRIMARY KEY (id)
 );
+
+CREATE TABLE IF NOT EXISTS clothes
+(
+    id          SERIAL      NOT NULL,
+    description VARCHAR(30) NOT NULL,
+    gender VARCHAR(2) NOT NULL,
+    size   VARCHAR(8),
+    PRIMARY KEY (id)
+);
+
+CREATE TABLE IF NOT EXISTS clothes_stock
+(
+    center_id   INTEGER NOT NULL,
+    clothing_id INTEGER NOT NULL,
+    PRIMARY KEY (clothing_id, center_id),
+    FOREIGN KEY (center_id) REFERENCES distribution_centers (id),
+    FOREIGN KEY (clothing_id) REFERENCES clothes (id)
+);
+
+
