db7d9d8d7b53d5fb9615ef6996df1ce823646ed9
adelsonsljunior <junioradelsonst@gmail.com>
2024-07-12 00:19:59 -0300

    feat: adding hygiene products to the database

--- database/db.sql
+++ database/db.sql
@@ -11,8 +11,8 @@ CREATE TABLE IF NOT EXISTS clothes
 (
     id          SERIAL      NOT NULL,
     description VARCHAR(30) NOT NULL,
-    gender VARCHAR(2) NOT NULL,
-    size   VARCHAR(8),
+    gender      VARCHAR(2)  NOT NULL,
+    size        VARCHAR(8),
     PRIMARY KEY (id)
 );
 
@@ -24,5 +24,24 @@ CREATE TABLE IF NOT EXISTS clothes_stock
     FOREIGN KEY (center_id) REFERENCES distribution_centers (id),
     FOREIGN KEY (clothing_id) REFERENCES clothes (id)
 );
+
+CREATE TABLE IF NOT EXISTS hygiene_products
+(
+    id          SERIAL      NOT NULL,
+    description VARCHAR(30) NOT NULL,
+    type VARCHAR(20) NOT NULL,
+    PRIMARY KEY (id)
+);
+
+CREATE TABLE IF NOT EXISTS hygiene_products_stock
+(
+    center_id   INTEGER NOT NULL,
+    hygiene_product_id INTEGER NOT NULL,
+    PRIMARY KEY (hygiene_product_id, center_id),
+    FOREIGN KEY (center_id) REFERENCES distribution_centers (id),
+    FOREIGN KEY (hygiene_product_id) REFERENCES hygiene_products (id)
+);
+
+
 
 
