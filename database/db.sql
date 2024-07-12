5ab066793ae024b77ec98ef64b83e86143af1578
adelsonsljunior <junioradelsonst@gmail.com>
2024-07-12 00:56:17 -0300

    feat: adding foods to the database

--- database/db.sql
+++ database/db.sql
@@ -29,18 +29,39 @@ CREATE TABLE IF NOT EXISTS hygiene_products
 (
     id          SERIAL      NOT NULL,
     description VARCHAR(30) NOT NULL,
-    type VARCHAR(20) NOT NULL,
+    type        VARCHAR(20) NOT NULL,
     PRIMARY KEY (id)
 );
 
 CREATE TABLE IF NOT EXISTS hygiene_products_stock
 (
-    center_id   INTEGER NOT NULL,
+    center_id          INTEGER NOT NULL,
     hygiene_product_id INTEGER NOT NULL,
     PRIMARY KEY (hygiene_product_id, center_id),
     FOREIGN KEY (center_id) REFERENCES distribution_centers (id),
     FOREIGN KEY (hygiene_product_id) REFERENCES hygiene_products (id)
 );
+
+CREATE TABLE IF NOT EXISTS foods
+(
+    id                  SERIAL      NOT NULL,
+    description         VARCHAR(30) NOT NULL,
+    quantity            INTEGER     NOT NULL,
+    unit_of_measurement VARCHAR(4)  NOT NULL,
+    validity            DATE        NOT NULL,
+    PRIMARY KEY (id)
+);
+
+CREATE TABLE IF NOT EXISTS foods_stock
+(
+    center_id INTEGER NOT NULL,
+    food_id   INTEGER NOT NULL,
+    PRIMARY KEY (food_id, center_id),
+    FOREIGN KEY (center_id) REFERENCES distribution_centers (id),
+    FOREIGN KEY (food_id) REFERENCES foods (id)
+);
+
+
 
 
 
